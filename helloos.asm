2c6863a465ab778b11e4ddb5b18dadd45f22e89c
hhiroki <gyouzakuuka@gmail.com>
2020-03-15 23:39:44 +0900

    helloos.asmの書き換え完了

--- helloos.asm
+++ helloos.asm
@@ -1,8 +1,13 @@
 ; hello-os
 ; TAB=4
 
-    DB      0xeb, 0x4e, 0x90
-    DB      "HELLOIPL"      ; ブートセレクタの名前を自由にかいていよい  (8Byte)
+    ORG     0x7c00          ; このプログラムが何処によみこまれるのか
+
+; 以下は標準的なFAT12フォーマットフロッピーディスクのための記述
+
+    JMP     entry
+    DB      0x90
+    DB      "HELLOIPL"
     DW      512             ; 1セクタの大きさ                           (512にしなければならない)
     DB      1               ; クラスタの大きさ                          (1セクタにしなければならない)
     DW      1               ; FATがどこから始まるか                     (普通は1セクタ目からにする)
@@ -23,27 +28,41 @@
 
 ; Program Main Body
 
-    DB  0xb8, 0x00, 0x00, 0x8e, 0xd0, 0xbc, 0x00, 0x7c
-    DB  0x8e, 0xd8, 0x8e, 0xc0, 0xbe, 0x74, 0x7c, 0x8a
-    DB  0x04, 0x83, 0xc6, 0x01, 0x3c, 0x00, 0x74, 0x09
-    DB  0xb4, 0x0e, 0xbb, 0x0f, 0x00, 0xcd, 0x10, 0xeb
-    DB  0xee, 0xf4, 0xeb, 0xfd
+entry:
+    MOV     AX,0            ; レジスタ初期化
+    MOV     SS,AX
+    MOV     SP,0x7c00
+    MOV     DS,AX
+    MOV     ES,AX
 
-; Message
+    MOV     SI,msg
+putloop:
+    MOV     AL,[SI]
+    ADD     SI,1            ; SIに1を足す
+    CMP     AL,0
+    JE      fin
+    MOV     AH,0x0e         ; 一文字表示ファンクション
+    MOV     BX,15           ; カラーコード
+    INT     0x10            ; ビデオBIOS呼び出し
+    JMP     putloop
+fin:
+    HLT                     ; 何かアルバでCPUを停止させる
+    JMP     fin             ; 無限ループ
 
-    DB      0x0a, 0x0a
+msg:
+    DB      0x0a, 0x0a      ; 改行を2つ
     DB      "hello, world"
-    DB      0x0a
+    DB      0x0a            ; 改行
     DB      0
 
-    RESB    0x1fe-($-$$)
-
+    ;RESB    0x7dfe-$        ; エラーになる。。。
+    RESB	0x7dfe-0x7c00-($-$$)		; 0x7dfeまでを0x00で埋める命令
     DB      0x55, 0xaa
 
-; ブート以外の記述
+
+; 以下はブートセクタ以外の部分の記述
 
     DB      0xf0, 0xff, 0xff, 0x00, 0x00, 0x00, 0x00, 0x00
     RESB    4600
     DB      0xf0, 0xff, 0xff, 0x00, 0x00, 0x00, 0x00, 0x00
     RESB    1469432
-
